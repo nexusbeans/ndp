ffed8952b3c4edc1ed8313c4d1565d3417047625
nexusbeans <nexusbeans@gmail.com>
2023-08-12 11:30:37 +0530

    boom boom

--- src/Components/GoogleCaptcha/GoogleCaptcha.jsx
+++ src/Components/GoogleCaptcha/GoogleCaptcha.jsx
@@ -3,18 +3,18 @@ import ReCAPTCHA from "react-google-recaptcha";
 
 
 export const GoogleCaptcha = () => {
-    const recaptchaRef = React.useRef();
+  const recaptchaRef = React.useRef();
 
-    const handleSubmit = async () => {
-      const token = await recaptchaRef.current.executeAsync();
-  
-    }
-  
+  const handleSubmit = async () => {
+    const token = await recaptchaRef.current.executeAsync();
+
+  }
+
   return (
     <ReCAPTCHA
-    ref={recaptchaRef}
-    size="invisible"
-    sitekey="6LdWYp0nAAAAALpcsq5rQ-ebXRuEnpXLC4opD16p"
-  />
+      ref={recaptchaRef}
+      size="invisible"
+      sitekey="6LdWYp0nAAAAALpcsq5rQ-ebXRuEnpXLC4opD16p"
+    />
   )
 }
