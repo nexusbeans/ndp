352dbbbf66c096d29718372dda56ac8a6ee850bf
nexusbeans <nexusbeans@gmail.com>
2023-08-16 13:47:44 +0530

    b

--- src/Components/UserReviews/ReadMore.jsx
+++ src/Components/UserReviews/ReadMore.jsx
@@ -1,34 +1,34 @@
-import React, { useState } from 'react';
-import { Button } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
+import React, { useState } from "react";
+import { Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
-function ReadMore({ text, showChar = 150 }) {
-    const [isExpanded, setIsExpanded] = useState(false);
+function ReadMore({ text, showChar = 160 }) {
+  const [isExpanded, setIsExpanded] = useState(false);
 
-    const toggleReadMore = () => {
-        setIsExpanded(!isExpanded);
-    };
+  const toggleReadMore = () => {
+    setIsExpanded(!isExpanded);
+  };
 
-    const content = isExpanded ? text : text.slice(0, showChar);
-    const shouldShowEllipsis = text.length > showChar && !isExpanded;
+  const content = isExpanded ? text : text.slice(0, showChar);
+  const shouldShowEllipsis = text.length > showChar && !isExpanded;
 
-    return (
-        <div>
-            <p>
-                {content}
-                {shouldShowEllipsis && <span className="moreellipses"> .....</span>}
-                {shouldShowEllipsis && (
-                    <span className="morecontent">
-                        <span>{text.slice(showChar)}</span>
-                      
-                        <Button className="morelink" onClick={toggleReadMore}>
-                            {isExpanded ? 'Show less' : 'Show more'}
-                        </Button>
-                    </span>
-                )}
-            </p>
-        </div>
-    );
+  return (
+    <div>
+      <p>
+        {content}
+        {shouldShowEllipsis && <span className="moreellipses"> .....</span>}
+        {shouldShowEllipsis && (
+          <span className="morecontent">
+            <span>{text.slice(showChar)}</span>
+
+            <Button className="morelink" onClick={toggleReadMore}>
+              {isExpanded ? "Show less" : "Show more"}
+            </Button>
+          </span>
+        )}
+      </p>
+    </div>
+  );
 }
 
 export default ReadMore;
